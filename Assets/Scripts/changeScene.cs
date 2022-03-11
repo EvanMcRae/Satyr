08b8fb3dbaca50902ec6cb45115997ddef8bb65e
Evtema3 <evanmmcrae@gmail.com>
2022-03-11 12:42:27 -0500

    Fix enemies knocking dead players into scene change

--- Assets/Scripts/changeScene.cs
+++ Assets/Scripts/changeScene.cs
@@ -33,7 +33,7 @@ public class changeScene : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && !Player.controller.dead)
         {
             StartCoroutine(LoadNextScene());
         }
