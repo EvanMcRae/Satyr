2b0409fd1b79ba84ee0929e89f842ad138b6954e
Evtema3 <evanmmcrae@gmail.com>
2022-04-01 17:11:18 -0400

    Leaf particles

---
+++ Assets/Scripts/LeafSpawn.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafSpawn : MonoBehaviour
+{
+    public Transform leaves;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            Vector3 currentPosition = leaves.transform.position;
+            currentPosition.x = Player.instance.transform.position.x;
+            leaves.position = currentPosition;
+
+            foreach (GameObject leaf in GameObject.FindGameObjectsWithTag("Leaf")) {
+                Debug.Log("bruh");
+                leaf.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+}
