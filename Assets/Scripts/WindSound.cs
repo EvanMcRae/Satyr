f3fe9d7d45bda4806e16db7e8df3df6a28b4990c
Evtema3 <evanmmcrae@gmail.com>
2022-04-06 12:58:45 -0400

    Wind sound demo

---
+++ Assets/Scripts/WindSound.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindSound : MonoBehaviour
+{
+    private bool triggeredEnter, triggeredExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player" && !triggeredEnter)
+        {
+            AudioManager.instance.FadeOutCurrent();
+            GetComponent<AudioSource>().Play();
+            triggeredEnter = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Player" && !triggeredExit)
+        {
+            AudioManager.instance.FadeInCurrent();
+            GetComponent<AudioSource>().Stop();
+            triggeredExit = true;
+        }
+    }
+}
