c4fb33576a9c3a56cbc22c06e1b37c6b502b720b
zacattac04 <zacharyjn@gmail.com>
2022-04-20 13:52:20 -0400

    Different Keyboard Schema
    Allowed Healing with 'A' and special attack with 'C'

--- Assets/MetroidvaniaController/Scripts/Player/Attack.cs
+++ Assets/MetroidvaniaController/Scripts/Player/Attack.cs
@@ -89,7 +89,7 @@ public class Attack : MonoBehaviour
                 StartCoroutine(ShootCooldown());
             }
 
-            if (Player.controller.specialAttack_Unlocked && !Statue.cutscening && specialCooldown >= specialMaxCooldown && (Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown("joystick button 3")))
+            if (Player.controller.specialAttack_Unlocked && !Statue.cutscening && specialCooldown >= specialMaxCooldown && (Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown("joystick button 3") || Input.GetKeyDown(KeyCode.C)))
             {
                 particleSpecialAttack.Play();
                 specialCooldown = 0.0f;
@@ -121,7 +121,7 @@ public class Attack : MonoBehaviour
             // 	special_attack_hitbox.enabled = false;
             // }
 
-            if ((Input.GetKeyUp(KeyCode.H) || Input.GetKeyUp("joystick button 5")) && cordyceps.count >= countToHeal && playerHealth.playerHealth < playerHealth.numberOfHearts)
+            if ((Input.GetKeyUp(KeyCode.H) || Input.GetKeyUp("joystick button 5") || Input.GetKeyUp(KeyCode.A)) && cordyceps.count >= countToHeal && playerHealth.playerHealth < playerHealth.numberOfHearts)
             {
                 playerHealth.playerHealth += 1;
                 cordyceps.count -= 5;
