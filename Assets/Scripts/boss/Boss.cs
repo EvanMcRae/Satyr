d4ec1850d801afcb72c7ab93527d0654b6f45100
Aidan-2020 <69286745+Aidan-2020@users.noreply.github.com>
2022-05-10 15:09:23 -0400

    boss and underground asset stuff
    added more functionality to the boss and also added more sprites to the underground

--- Assets/Scripts/boss/Boss.cs
+++ Assets/Scripts/boss/Boss.cs
@@ -28,13 +28,13 @@ public class Boss : MonoBehaviour
         Vector3 flipped = transform.localScale;
         flipped.z *= -1f;
 
-        if(transform.position.x > player.position.x && isFlipped)
+        if(transform.position.x < player.position.x && isFlipped)
         {
             transform.localScale = flipped;
             transform.Rotate(0f, 180f, 0f);
             isFlipped = false;
         }
-        else if(transform.position.x < player.position.x && !isFlipped)
+        else if(transform.position.x > player.position.x && !isFlipped)
         {
             transform.localScale = flipped;
             transform.Rotate(0f, 180f, 0f);
@@ -44,8 +44,10 @@ public class Boss : MonoBehaviour
 
     public void Attack()
     {
+        print("boss attacks");
         Vector3 pos = transform.position;
-        pos += transform.right * attackOffset.x;
+        //pos += transform.right * attackOffset.x;
+        pos += transform.right * -attackOffset.x;
         pos += transform.up * attackOffset.y;
 
         Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
