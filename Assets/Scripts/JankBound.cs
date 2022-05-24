555941417acf2fcd307a3a43a95594d1123bb400
EvanMcRae <evanmmcrae@gmail.com>
2022-05-24 11:51:33 -0400

    Fix bound

---
+++ Assets/Scripts/JankBound.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JankBound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
