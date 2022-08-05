69ee1a144a5e8db1166638308da0cebdd9d3ff13
Aidan-2020 <69286745+Aidan-2020@users.noreply.github.com>
2022-08-05 16:31:26 -0400

    glow mush
    added glowing mushroom enemy to f6

---
+++ Assets/Scripts/GlowMushroom.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowMushroom : Enemy
+{
+    public Animator mushAnim;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Player.instance.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.position.x > this.transform.position.x && !facingRight)
+        {
+            Flip();
+        }
+        else if (target.position.x < this.transform.position.x && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            print("player entered radius");
+            mushAnim.SetBool("isAttacking", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            mushAnim.SetBool("isAttacking", false);
+            print("player exited");
+        };
+    }
+
+}
