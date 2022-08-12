117146ffee37b5eb707f24c7d5bea01b0aafb6f9
Aidan-2020 <69286745+Aidan-2020@users.noreply.github.com>
2022-08-12 15:35:47 -0400

    glow mush
    glowing mushroom enemy / obstacle is finished !

--- Assets/Scripts/GlowMushroom.cs
+++ Assets/Scripts/GlowMushroom.cs
@@ -6,6 +6,10 @@ public class GlowMushroom : Enemy
 {
     public Animator mushAnim;
     public Transform target;
+
+    public Vector3 attackOffset;
+    public float attackRange = 1f;
+    public LayerMask attackMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,6 +45,22 @@ public class GlowMushroom : Enemy
             mushAnim.SetBool("isAttacking", false);
             print("player exited");
         };
+    }
+
+    public void Attack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * -attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null)
+        {
+            if (colInfo.tag == "Player")
+            {
+                colInfo.GetComponent<Player>().ApplyDamage(1.0f, this.transform.position, 30f);
+            }
+        }
     }
 
 }
