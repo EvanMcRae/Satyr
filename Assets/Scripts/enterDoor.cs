18a57d2944faa19e47cd5779d3edd8c1a0626f3f
EvanMcRae <evanmmcrae@gmail.com>
2022-08-17 16:58:56 -0400

    Fix info box for doors

--- Assets/Scripts/enterDoor.cs
+++ Assets/Scripts/enterDoor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class enterDoor : MonoBehaviour
@@ -13,6 +14,8 @@ public class enterDoor : MonoBehaviour
     public string scene;
     public string spawn;
     private Animator crossfade;
+    public GameObject InfoBox, InfoText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,27 +32,26 @@ public class enterDoor : MonoBehaviour
         }
     }
 
-
-    public void OnGUI()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (!PlayerMovement.paused && playerIsInRange)
-        {
-            GUI.Box(new Rect(140, Screen.height - 50, Screen.width - 300, 120), (labelText));
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
+        if (!PlayerMovement.paused && collision.gameObject.tag == "Player" && InfoText.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("textfade_hold"))
         {
             playerIsInRange = true;
-            labelText = "Press B button or T key to Enter";
+            labelText = "Press B button or T key to enter.";
+            InfoText.GetComponent<Text>().text = labelText;
+            InfoText.GetComponent<Animator>().SetTrigger("start");
+            InfoBox.GetComponent<Animator>().SetTrigger("start");
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         playerIsInRange = false;
+        if (InfoText.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("textfade_start"))
+        {
+            InfoText.GetComponent<Animator>().SetTrigger("stop");
+            InfoBox.GetComponent<Animator>().SetTrigger("stop");
+        }
     }
 
     IEnumerator LoadNextScene()
